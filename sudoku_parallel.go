e1d162c44038dc00a9f39bac68008f2b4e213846
Mike Smith <mike@sticknet.net>
2013-05-02 00:32:34 -0500

    Name anonymous function as loadBoards(string)

--- sudoku_parallel.go
+++ sudoku_parallel.go
@@ -36,22 +36,14 @@ func collectResults(solutions chan *sudoku.Solution) {
 	fmt.Printf("Solved %v puzzles in %v (%0.2f per second)\n", count, elapsed, rate)
 }
 
-func waitForDone(workers int, done chan bool, toClose chan *sudoku.Solution) {
+func waitForSolvers(workers int, done chan bool, toClose chan *sudoku.Solution) {
 	for i := 0; i < workers; i++ {
 		<- done
 	}
 	close(toClose)
 }
 
-func main() {
-	var fName string
-
-	if len(os.Args) > 1 {
-		fName = os.Args[1]
-	} else {
-		fName = "sets/sudoku17.gz"
-	}
-
+func loadBoards(fName string, unsolved chan *sudoku.Board) {
 	file, err := os.Open(fName)
 	if err != nil {
 		log.Fatal(err)
@@ -64,8 +56,26 @@ func main() {
 	}
 	reader := bufio.NewReader(gz_reader)
 
+	for {
+		b, err := sudoku.ReadBoardLine(reader)
+		if err != nil {
+			break
+		}
+		unsolved <- &b
+	}
+	close(unsolved)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		println("Usage: sudoku_parallel [file]")
+		os.Exit(1)
+	}
+
 	workers := runtime.NumCPU()
 	runtime.GOMAXPROCS(workers)
+
+	fName := os.Args[1]
 
 	unsolved := make(chan *sudoku.Board)
 	solved := make(chan *sudoku.Solution)
@@ -75,18 +85,9 @@ func main() {
 		go solver(unsolved, solved, done)
 	}
 
-	go func() {
-		for {
-			b, err := sudoku.ReadBoardLine(reader)
-			if err != nil {
-				break
-			}
-			unsolved <- &b
-		}
-		close(unsolved)
-	}()
+	go loadBoards(fName, unsolved)
 
-	go waitForDone(workers, done, solved)
+	go waitForSolvers(workers, done, solved)
 
 	collectResults(solved)
 }
